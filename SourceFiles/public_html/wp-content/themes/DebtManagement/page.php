ab8745851d7e9789158737f2a8a2f968853b95bf
Juls <Juls@JulsLaptop>
2014-06-10 11:39:03 -0500

    updated page

--- SourceFiles/public_html/wp-content/themes/DebtManagement/page.php
+++ SourceFiles/public_html/wp-content/themes/DebtManagement/page.php
@@ -1,5 +1,32 @@
 <?php
 /*
-Template Name: Default Page
-*/
-?>
+  Template Name: Default Page
+ */
+
+$currentPage = get_queried_object();
+
+$pageID = $currentPage->ID;
+global $bodyId;
+$bodyId = '';
+get_header();
+
+// Set up the objects needed
+$pagesQuery = new WP_Query();
+$allPages = $pagesQuery->query(array('post_type' => 'page', 'posts_per_page' => -1));
+
+    ?>
+        <div class="main">
+            <div class="inside">
+                <section id="content">
+                    <div class="container">
+                        <h2><?php echo $currentPage->post_title; ?></h2>
+                        <div class="txt">
+                            <?php echo $currentPage->post_content; ?>
+                        </div>                        
+                    </div> 
+                </section>
+            </div>
+        </div>
+    <?php
+    get_footer();
+    ?>
