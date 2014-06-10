4cc570e3d8bb31af4b1a59f47393b5033c6e03a8
Juls <Juls@JulsLaptop>
2014-06-10 13:11:41 -0500

    using custom fields for links

--- SourceFiles/public_html/wp-content/themes/DebtManagement/footer.php
+++ SourceFiles/public_html/wp-content/themes/DebtManagement/footer.php
@@ -1,3 +1,10 @@
+<?php
+$homePageObject = new WP_Query( 'name=home&post_type=page' );
+$homePageId = $homePageObject->post->ID;
+
+//echo '$homePageId is '.$homePageId;
+?>
+
         <footer>      
             <div class="main">
                 <div class="inside">
@@ -8,23 +15,98 @@
                             <a href="index-6.html">Privacy policy</a> <br>
                             <!--{%FOOTER_LINK}-->
                         </div>
+
+                        <?php
+                        // check if the repeater field has rows of data
+                        if( have_rows('footer_why_us_links', $homePageId) ):
+                            ?>
                         <div class="col-2">
                             <h2>Why Us</h2>
-                            <ul>
-                                <li><a href="#">Our Clients</a></li>
-                                <li><a href="#">Technology</a></li>
-                                <li><a href="#">Career</a></li>
+                            <ul>                        
+                            <?php
+                                // loop through the rows of data
+                            while ( have_rows('footer_why_us_links', $homePageId) ) : the_row();
+                                // display a sub field value
+                                if( get_sub_field('why_us_link_type', $homePageId) == 'Enter URL'){
+                                    $linkUrl = get_sub_field('why_us_link_url', $homePageId);
+                                }
+                                elseif( get_sub_field('why_us_link_type', $homePageId) == 'Select Page/Post'){
+                                    $linkUrl = get_sub_field('why_us_linked_page', $homePageId);
+                                }
+                                ?>
+                                <li><a href="<?php echo $linkUrl; ?>"><?php the_sub_field('why_us_link_name', $homePageId); ?></a></li>
+                                <?php
+                            endwhile;
+                        ?>
                             </ul>
-                        </div>
+                        </div>                                
+                        <?php
+                        else :
+                            // no rows found
+                        endif;
+                        ?>       
+                        
+                        <?php
+                        // check if the repeater field has rows of data
+                        if( have_rows('footer_quick_links', $homePageId) ):
+                            ?>
                         <div class="col-3">
                             <h2>Quick Links</h2>
-                            <ul>
-                                <li><a href="#">Lorem ipsum dolor </a></li>
-                                <li><a href="#">Amet consectetuer </a></li>
-                                <li><a href="#">Mauris vel tortor vitae </a></li>
-                                <li><a href="#">Nibh condimentum </a></li>
+                            <ul>                        
+                            <?php
+                                // loop through the rows of data
+                            while ( have_rows('footer_quick_links', $homePageId) ) : the_row();
+                                // display a sub field value
+                                if( get_sub_field('footer_quick_links_type', $homePageId) == 'Enter URL'){
+                                    $linkUrl = get_sub_field('footer_quick_links_url', $homePageId);
+                                }
+                                elseif( get_sub_field('footer_quick_links_type', $homePageId) == 'Select Page/Post'){
+                                    $linkUrl = get_sub_field('footer_quick_links_linked_page', $homePageId);
+                                }
+                                ?>
+                                <li><a href="<?php echo $linkUrl; ?>"><?php the_sub_field('footer_quick_links_name', $homePageId); ?></a></li>
+                                <?php
+                            endwhile;
+                        ?>
                             </ul>
-                        </div>
+                        </div>                                
+                        <?php
+                        else :
+                            // no rows found
+                        endif;
+                        ?>     
+                        
+                        
+                        <?php
+                        // check if the repeater field has rows of data
+                        if( have_rows('footer_clients', $homePageId) ):
+                            ?>
+                        <div class="col-3">
+                            <h2>Clients</h2>
+                            <ul>                        
+                            <?php
+                                // loop through the rows of data
+                            while ( have_rows('footer_clients', $homePageId) ) : the_row();
+                                // display a sub field value
+                                if( get_sub_field('footer_clients_link_type', $homePageId) == 'Enter URL'){
+                                    $linkUrl = get_sub_field('footer_clients_url', $homePageId);
+                                }
+                                elseif( get_sub_field('footer_clients_link_type', $homePageId) == 'Select Page/Post'){
+                                    $linkUrl = get_sub_field('footer_clients_linked_page', $homePageId);
+                                }
+                                ?>
+                                <li><a href="<?php echo $linkUrl; ?>"><?php the_sub_field('footer_clients_name', $homePageId); ?></a></li>
+                                <?php
+                            endwhile;
+                        ?>
+                            </ul>
+                        </div>                                
+                        <?php
+                        else :
+                            // no rows found
+                        endif;
+                        ?>   
+
                         <div class="col-3">
                             <h2>Clients</h2>
                             <ul>
